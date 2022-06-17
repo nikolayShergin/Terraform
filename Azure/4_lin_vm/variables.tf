740c38f74157fb3257cee6124b50ac9bdc3921c4
NikolayShergin <nikolay.shergin@gmail.com>
2022-06-17 14:03:27 +0200

    vm

---
+++ Azure/4_lin_vm/variables.tf
@@ -0,0 +1,12 @@
+variable "rg_name" {
+  type        = string
+  description = "name of the resource group"
+}
+variable "deploy_location" {
+  type        = string
+  description = "deployment location for the resources"
+  default     = "germanywestcentral"
+}
+variable "my_nic_name" {
+  
+}
